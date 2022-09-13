6ce6533e1caf92538f68bd92d1c7eb54ed39ad81
Mwanza <molo.mwanza1@students.jkuat.ac.ke>
2022-09-14 01:56:05 +0300

    file update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,11 +14,12 @@ void print_times_table(int n)
 			{
 				_putchar('0');
 				for (mult = 1; mult <= n; mult++)
-   				{
+				{
 					_putchar(',');
 					_putchar(' ');
 					prod = num * mult;
-					if (prod <= 99)	   																				_putchar(' ');
+					if (prod <= 99)				
+						_putchar(' ')
 					if (prod <= 9)																					_putchar(' ');
 					if (prod >= 100)
 					{
