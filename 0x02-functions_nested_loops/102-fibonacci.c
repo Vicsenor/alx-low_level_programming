b8767a2be72572fe899a4c63b7e82b990199968e
Mwanza <molo.mwanza1@students.jkuat.ac.ke>
2022-09-14 01:37:56 +0300

    file update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (count = 0; count < 50; count++)
 	{
-			sum = fib1 + fib2;
-			printf("%lu", sum);
-			fib1 = fib2;
-			fib2 = sum;
-			if (count == 49)
-				printf("\n");
-			else
-				printf(", ");
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+		fib1 = fib2;
+		fib2 = sum;
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
 return (0);
 }
