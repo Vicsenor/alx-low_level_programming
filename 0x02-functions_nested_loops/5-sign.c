3e66e307c37afde048b15bab8c363ec591000c4e
Mwanza <molo.mwanza1@students.jkuat.ac.ke>
2022-09-13 23:07:51 +0300

    file updates

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,11 +6,12 @@
 *     *Return: 1 and print + if n > 1
 *      * 0 and print 0 if n = 0
 *       * -1 and print - if n < 0
-*        */
+*/
 int print_sign(int n)
 
 {
 	int result;
+
 	if (n > 0)
 	{
 		_putchar('+');
