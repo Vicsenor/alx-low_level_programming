cdad27e219a2a892b8bc3ba8d8fd931625bca7e8
Mwanza <molo.mwanza1@students.jkuat.ac.ke>
2022-09-16 02:14:20 +0300

    file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,24 @@
 #include "main.h"
 /**
-* more_numbers - prints numbers between 0 to 14
-* 10 times.
-* Return: no return.
-*/
+* *more_numbers - prints numbers to 14
+* *
+* *Return: returns nothing
+* */
 void more_numbers(void)
 {
-	int i, ch;
+	int number1, number2;
 
-	for (i = 0; i < 10; i++)
+	for (number1 = 0; number1 < 10; number1++)
 	{
-		for (ch = 0; ch < 15; ch++)
+		for (number2 = 0; number2 <= 14; number2++)
+
 		{
-			if (ch >= 10)
-				_putchar((ch / 10) + 48);
-			_putchar((ch%10)+48)
+			if (number2 > 9)
+			{
+				_putchar((number2 / 10) + '0');
+			}
+			_putchar((number2 % 10) + '0');
 		}
-		_putchar('\n');
+		_putchar(10);
 	}
 }
